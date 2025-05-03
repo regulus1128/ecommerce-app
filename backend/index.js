7d33a7ca9a689df2341b065416a03a1b3fbbefd9
Suraj Shyam <102654234+regulus1128@users.noreply.github.com>
2025-05-03 21:39:34 +0530

    changed cors

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ connectDb();
 connectCloudinary();
 
 app.use(cors({
-    origin: ["http://localhost:5174", "http://localhost:5173"],
+    origin: ["https://solevibe-admin.vercel.app", "https://solevibe-frontend.vercel.app"],
     credentials: true
 }));
 
